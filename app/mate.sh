ba04276418136431c838e9564f80aad1ce0005c7
harry-1104 <132640215+harry-1104@users.noreply.github.com>
2023-10-14 06:36:48 +0000

    nothing

--- app/mate.sh
+++ app/mate.sh
@@ -3,6 +3,7 @@
 
 apt update -y && apt upgrade -y
 apt install ubuntu-mate-desktop mate-desktop-environment mate-indicator-applet-common mate-indicator-applet -y
+rm -rf ~/.config/mate
 # Or install mate-desktop-environment
 
 ### Command: startx mate-session
