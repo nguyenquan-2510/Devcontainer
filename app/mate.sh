4a5c4bb1e2fb4d107a083989774e33f08097acf9
harry-1104 <132640215+harry-1104@users.noreply.github.com>
2023-10-14 06:35:23 +0000

    successfully use ubuntu mate desktop but its panel disappear immediately

--- app/mate.sh
+++ app/mate.sh
@@ -2,7 +2,7 @@
 
 
 apt update -y && apt upgrade -y
-apt install ubuntu-mate-desktop -y
+apt install ubuntu-mate-desktop mate-desktop-environment mate-indicator-applet-common mate-indicator-applet -y
 # Or install mate-desktop-environment
 
 ### Command: startx mate-session
