0b7d1e8481c12230aeb6d85204618a5aa55f7085
harry-1104 <132640215+harry-1104@users.noreply.github.com>
2023-10-28 12:36:20 +0000

    ST

--- linux-rdp.sh
+++ linux-rdp.sh
@@ -1,11 +1,11 @@
-# Kali xrdp
+# Xrdp desktop
 # Documentation: https://techspan.org/setup-kali-linux-via-docker-for-rdp-xfce-remote-control/
-docker pull ubuntu
-docker run -p 3389:3389 -it --name linux-rdp ubuntu /bin/bash
+docker pull kalilinux/kali-rolling
+docker run -p 3389:3389 -it --name linux-rdp kalilinux/kali-rolling /bin/bash
 ## Command in container:
 # su
 # apt update
-# apt-get install -y dbus-x11 kali-desktop-xfce xrdp
+# apt-get install -y dbus-x11 kali-desktop-xfce xrdp software-properties-common
 # apt-get purge -y xfce4-power-manager
 # service xrdp start
 # passwd
