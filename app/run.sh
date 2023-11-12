edd32defd85bb2f7f4bef4998b2b5d8a018dd258
harry-1104 <132640215+harry-1104@users.noreply.github.com>
2023-11-12 05:23:45 +0000

    hehhe

--- app/run.sh
+++ app/run.sh
@@ -1,4 +1,4 @@
 clear
 docker build -t vnc-app .
-docker run -d --shm-size 3g --name vnc-container -p 80:80 vnc-app
+docker run -d --shm-size 3g --name vnc-container -p 8080:80 vnc-app
 docker exec -it vnc-container bash
