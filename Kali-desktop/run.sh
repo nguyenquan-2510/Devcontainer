ee51e98e1eab09300861b4ac0ad7c91c310302ee
harry-1104 <132640215+harry-1104@users.noreply.github.com>
2023-11-13 07:12:01 +0000

    hh

--- Kali-desktop/run.sh
+++ Kali-desktop/run.sh
@@ -1,4 +1,4 @@
 clear
 docker build -t kali . && \
-docker run -d --shm-size 3g -v ./kali-data:/root --name kali-container -p 8080:80 kali && \
+docker run -d --shm-size 3g --name kali-container -p 8080:80 kali && \
 docker exec -it kali-container bash
